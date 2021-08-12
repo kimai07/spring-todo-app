3531462c86eac7a74060df4f045dbb76d552b21f
kenji imai <imakenfever@gmail.com>
2021-08-12 22:09:29 +0900

    Format codes

--- src/main/java/com/kimai07/todoapp/controller/HelloController.java
+++ src/main/java/com/kimai07/todoapp/controller/HelloController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1")
 public class HelloController {
 
-  @GetMapping("/hello")
-  public String hello() {
-    return "Hello World!";
-  }
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
 }
